48fc24913c40e2b5e8f0cedcb842c714cf62c031
Jaime-gc-00 <78904735+Jaime-gc-00@users.noreply.github.com>
2022-05-11 22:32:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from Flask import current_app, Flask, render_template
 import json
 import os
 
-app = Flask(__name__)
+ROOT_PREFIX = "/app"
+app = Flask(__name__, static_url_path=ROOT_PREFIX + '/static')
+
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(APP_DIR, 'templates')
+STATIC_DIR = os.path.join(APP_DIR,'static')
+current_app.template_folder = TEMPLATE_DIR
+current_app.static_folder = STATIC_DIR
 
+@app.route('/'+ROOT_PREFIX)
 def main():
-    current_app.template_folder = TEMPLATE_DIR
-    if false:
-        reorderquant =request.form.get('reorderquant')
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
